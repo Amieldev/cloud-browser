4e489e85341ab3f1b1f821a7234f5acc62e5a71c
Amiel Sintayehu <amielsentayehu@gmail.com>
2022-02-09 22:34:07 +0300

    Changed port name

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const express=require('express');
-const app=express();
-const port=process.env.PORT||7000;
-const {Search}=require('./functions/search');
-const {SSR}=require('./functions/ssr');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const { Search } = require('./functions/search');
+const { SSR } = require('./functions/ssr');
 
 app.use(express.static('web'));
 app.use(express.json());
 
-app.get('/ssr',(req,res)=>{
-    (async ()=>{
-        const html =await SSR('http://localhost:5000');
+app.get('/', (req, res) => {
+
+});
+
+app.get('/ssr', (req, res) => {
+    (async () => {
+        const html = await SSR('http://localhost:5000');
         console.log(html);
         res.send(html);
     })();
 });
 
-app.post('/ask',async (req,res)=>{
-    const {text}=req.body;
+app.post('/ask', async (req, res) => {
+    const { text } = req.body;
     console.log(`quest:${text}`);
-    const answer=await Search(text);
+    const answer = await Search(text);
     console.log(`answer:${answer}`);
-    res.json({text:answer});
+    res.json({ text: answer });
 });
 
-app.listen(port,()=>console.log(`Opened at port:${port}`));
+app.listen(port, () => console.log(`Opened at port:${port}`));
