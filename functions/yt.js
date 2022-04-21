324b64f6c3b6270dac498ec519cb9ce3ebdc6720
Amiel Sintayehu <amielsentayehu@gmail.com>
2022-04-21 18:27:56 +0300

    Increased no. of responses for yt function

--- functions/yt.js
+++ functions/yt.js
@@ -5,11 +5,23 @@ async function YT(title){
     const page = await browser.newPage();
     await page.goto(`https://www.youtube.com/results?search_query=${title}`);
     await page.waitForSelector('.yt-simple-endpoint.style-scope.ytd-video-renderer');
-    const link=await page.evaluate(()=>{
-        return document.querySelector('.yt-simple-endpoint.style-scope.ytd-video-renderer').href;
+    const links=await page.evaluate(()=>{
+        const videos=Array.from(document.querySelectorAll('.yt-simple-endpoint.style-scope.ytd-video-renderer'));
+    
+        const urls=[];
+
+         videos.forEach(video=>{
+            if(videos.indexOf(video)<100&&video.href!==""){
+                urls.push(video.href);
+            }
+        });
+
+        return urls
+
+
     });
     await browser.close();
-    return link;
+    return links;
 };
 
 module.exports={
