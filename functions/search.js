f4f2830e8c6ad71a191334959f97e6d117313843
Amiel Sintayehu <amielsentayehu@gmail.com>
2022-04-29 19:08:56 +0300

    Adde more sources to search function

--- functions/search.js
+++ functions/search.js
@@ -5,14 +5,19 @@ async function Search(quest){
     const page=await browser.newPage();
     await page.goto(`https://www.google.com/search?q=${quest}`);
     const answer=await page.evaluate(() => {
-    const mainArticle=document.querySelector('.hgKElc');
-        const miniArticle=document.querySelector('.kno-rdesc').querySelector('span');
-        if(mainArticle!==null){
+        const mainArticle=document.querySelector('.hgKElc');
+        const miniArticle=document.querySelector('.kno-rdesc span');
+        const microArticle=document.querySelector('.VwiC3b.yXK7lf.MUxGbd.yDYNvb.lyLwlc.lEBKkf span');
+        if(mainArticle!==null&&mainArticle!==undefined){
             return mainArticle.innerText;
         }
-        else if(miniArticle!==null){
+        else if(miniArticle!==null&&miniArticle!==undefined){
             return miniArticle.innerText
-        }else{
+        }
+        else if(microArticle!==null&&microArticle!==undefined){
+            return microArticle.innerText
+        }
+        else{
             return "Couldn't find the result,try rephrasing the question";
         }
 });
