b3fba2c87a9ea2e7a557d5abe4f37a260d320807
Amiel Sintayehu <amielsentayehu@gmail.com>
2022-05-01 20:08:05 +0300

    Added auto lang detect to translate function

--- functions/translate.js
+++ functions/translate.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 async function Translate(lang,text){
     const browser = await puppeteer.launch({ headless:true});
     const page = await browser.newPage();
-    await page.goto(`https://translate.google.com.et/?hl=en&tab=rT&sl=en&tl=${lang}&text=${text}&op=translate`);
+    await page.goto(`https://translate.google.com.et/?hl=en&tab=wT&sl=auto&tl=${lang}&text=${text}&op=translate`);
     await page.waitForSelector(".J0lOec");
     const answer = await page.evaluate(() => {
         return document.querySelector(".J0lOec").innerText;
