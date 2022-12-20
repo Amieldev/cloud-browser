4f3590fa0011128a1dc9e24e6e0c319e0afdd26e
Amieldev <amielsentayehu@gmail.com>
2022-12-20 15:13:39 +0300

    Fixed selector issue

--- functions/translate.js
+++ functions/translate.js
@@ -4,9 +4,9 @@ async function Translate(lang,text){
     const browser = await puppeteer.launch({ headless:true});
     const page = await browser.newPage();
     await page.goto(`https://translate.google.com.et/?hl=en&tab=wT&sl=auto&tl=${lang}&text=${text}&op=translate`);
-    await page.waitForSelector(".J0lOec");
+    await page.waitForSelector(".ryNqvb");
     const answer = await page.evaluate(() => {
-        return document.querySelector(".J0lOec").innerText;
+        return document.querySelector(".ryNqvb").innerText;
     });
     await browser.close();
     return answer;
